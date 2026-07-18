32e1e4d75510f744b6d112209ddf772a997755ac
Shivam16Gupta <shivamgupta1281@gmail.com>
2026-07-19 00:37:20 +0530

    separated data input and embedding

---
+++ app.py
@@ -0,0 +1,10 @@
+from src.data_loader import load_all_documents
+from src.embedding import EmbeddingPipeline
+
+
+if __name__ == "__main__":
+    docs=load_all_documents("data")
+    chunks=EmbeddingPipeline().chunk_documents(docs)
+    chunkvectors=EmbeddingPipeline().embed_chunks(chunks)
+    print(chunkvectors)
+    
