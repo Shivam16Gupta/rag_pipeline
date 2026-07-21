051ccd44a066c0945a706681eeca64be141afbc1
Shivam16Gupta <shivamgupta1281@gmail.com>
2026-07-21 18:41:27 +0530

    grop llm updated

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from src.data_loader import load_all_documents
-from src.embedding import EmbeddingPipeline
-
+from src.vectorstore import FaissVectorStore
+from src.search import RAGSearch
 
 if __name__ == "__main__":
-    docs=load_all_documents("data")
-    chunks=EmbeddingPipeline().chunk_documents(docs)
-    chunkvectors=EmbeddingPipeline().embed_chunks(chunks)
-    print(chunkvectors)
+    # docs=load_all_documents("data")
+    store=FaissVectorStore("faiss_store")
+    # store.build_from_documents(docs)
+    store.load()
+    # print(store.query("What is attention mechanism?", top_k=3))
     
+    rag_search=RAGSearch()
+    query="What is Deep Bidirectional Transformers?"
+    summary=rag_search.search_and_summarize(query,top_k=3)
+    print("Summary:",summary)
